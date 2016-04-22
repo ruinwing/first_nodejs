4a7f9db761f0c28d09bbc76ad83edd896ef54809
ruinwing <ruinwing@nate.com>
2016-04-22 17:14:48 +0900

    post data test

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,20 @@
+var querystring = require("querystring");
+
 function start(response){
   console.log("Request handler 'start' was called.");
-  setTimeout(function(){
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write("hello Start");
-    response.end();
-  }, 10000);
+  response.writeHead(200, {"Content-Type" : "text/plain"});
 
+  var body = "<html><head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8' /></head><body><form action = '/hello' method = 'post'><input type = 'text' name = 'text' /> <input type = 'submit' value = '입력' /></form></body></html>";
+
+  response.writeHead(200, {"COntent-Type" : "text/html"});
+  response.write(body);
+  response.end();
 }
 
-function hello(response){
+function hello(response, postData){
   console.log("Request handler 'hello' was called.");
   response.writeHead(200, {"Content-Type" : "text/plain"});
-  response.write("hello, World!");
+  response.write("hello, " + querystring.parse(postData).text);
   response.end();
 }
 
